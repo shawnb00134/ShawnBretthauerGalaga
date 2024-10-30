9f10b8ff4067f0aa542abf076d069bd3ea926f0c
Shawn Bretthauer <sb00134@my.westga.edu>
2024-10-30 15:53:06 -0400

    Made EnemyManager class to handle creation and movement.

---
+++ Galaga/Model/EnemyManager.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Galaga.Model
+{
+    public class EnemyManager
+    {
+        private Canvas canvas;
+
+        public EnemyManager(Canvas canvas)
+        {
+            this.canvas = canvas;
+        }
+
+        public List<EnemyShip> CreateAndPlaceEnemyShip()
+        {
+            List<EnemyShip> enemyShips = new List<EnemyShip>();
+
+            double canvasWidth = this.canvas.Width;
+            int[] enemiesPerRow = { 2, 3, 4 };
+            double startY = 200;
+            double rowSpacing = 100;
+
+            for (int rowIndex = 0; rowIndex < enemiesPerRow.Length; rowIndex++)
+            {
+                int enemyCount = enemiesPerRow[rowIndex];
+                double spacing = canvasWidth / (enemyCount + 1);
+
+                for (int i = 0; i < enemyCount; i++)
+                {
+                    EnemyShip enemyShip;
+                    if (rowIndex == 0)
+                    {
+                        enemyShip = new EnemyLevel1();
+                    }
+                    else if (rowIndex == 1)
+                    {
+                        enemyShip = new EnemyLevel2();
+                    }
+                    else
+                    {
+                        enemyShip = new EnemyLevel3();
+                    }
+
+                    this.canvas.Children.Add(enemyShip.Sprite);
+                    enemyShips.Add(enemyShip);
+
+                    double xPosition = (i + 1) * spacing - enemyShip.Width / 2.0;
+                    enemyShip.X = xPosition;
+                    enemyShip.Y = startY - rowIndex * rowSpacing;
+                }
+            }
+
+            return enemyShips;
+        }
+
+        /// <summary>
+        /// Moves the enemy ships.
+        /// </summary>
+        /// <param name="enemyShips">The enemy ships.</param>
+        /// <param name="tickCounter">The tick counter.</param>
+        public void MoveEnemyShips(List<EnemyShip> enemyShips, int tickCounter)
+        {
+            
+            foreach (var ship in enemyShips)
+            {
+                if (ship == null)
+                {
+                    System.Diagnostics.Debug.WriteLine(tickCounter);
+                }
+
+                if (tickCounter < 10)
+                {
+                    ship.MoveLeft();
+                }
+                else if (tickCounter < 30)
+                {
+                    ship.MoveRight();
+                }
+                else if (tickCounter < 40)
+                {
+                    ship.MoveLeft();
+                }
+                
+            }
+        }
+    }
+}
--- Galaga/Model/GameManager.cs
+++ Galaga/Model/GameManager.cs
@@ -13,22 +13,27 @@ namespace Galaga.Model
     {
         #region Data members
 
-        private readonly List<EnemyShip> enemyShips;
+        private List<EnemyShip> enemyShips;
         private readonly List<GameObject> listOfShips;
         private readonly List<GameObject> missiles;
 
         private const double PlayerOffsetFromBottom = 30;
+        private const int PlayerSpeedBoundary = 3;
         private readonly Canvas canvas;
         private readonly double canvasHeight;
         private readonly double canvasWidth;
 
         private readonly Random random;
         private readonly DispatcherTimer timer;
+        private const int tickTimer = 50;
         private int tickCounter;
+        private const int tickCounterReset = 40;
         private int playerMissileCount;
 
         private Player player;
         private Physics physics;
+        private EnemyManager enemyManager;
+
 
         #endregion
 
@@ -48,6 +53,7 @@ namespace Galaga.Model
             this.enemyShips = new List<EnemyShip>();
             this.listOfShips = new List<GameObject>();
             this.missiles = new List<GameObject>();
+            this.enemyManager = new EnemyManager(this.canvas);
 
             this.initializeGame();
 
@@ -57,7 +63,7 @@ namespace Galaga.Model
             this.physics = new Physics();
 
             this.timer = new DispatcherTimer();
-            this.timer.Interval = new TimeSpan(0, 0, 0, 0, 50);
+            this.timer.Interval = new TimeSpan(0, 0, 0, 0, tickTimer);
             this.timer.Tick += this.timer_Tick;
             this.timer.Start();
         }
@@ -66,11 +72,11 @@ namespace Galaga.Model
         {
             this.enemyFireMissiles();
             this.physics.CheckMissileBoundary(this.missiles, this.canvas);
-            this.moveEnemyShips();
+            this.enemyManager.MoveEnemyShips(this.enemyShips, this.tickCounter);
             this.moveMissiles();
             this.tickCounter++;
             
-            if (this.tickCounter >= 40)
+            if (this.tickCounter >= tickCounterReset)
             {
                 this.tickCounter = 0;
             }
@@ -85,7 +91,8 @@ namespace Galaga.Model
         private void initializeGame()
         {
             this.createAndPlacePlayer();
-            this.createAndPlaceEnemyShip();
+            //this.createAndPlaceEnemyShip();
+            this.enemyShips = this.enemyManager.CreateAndPlaceEnemyShip();
         }
 
         private void createAndPlacePlayer()
@@ -97,43 +104,7 @@ namespace Galaga.Model
             this.placePlayerNearBottomOfBackgroundCentered();
         }
 
-        private void createAndPlaceEnemyShip()
-        {
-            double canvasWidth = this.canvas.Width;
-            int[] enemiesPerRow = { 2, 3, 4 };
-            double startY = 200;
-            double rowSpacing = 100;
-
-            for (int rowIndex = 0; rowIndex < enemiesPerRow.Length; rowIndex++)
-            {
-                int enemyCount = enemiesPerRow[rowIndex];
-                double spacing = canvasWidth / (enemyCount + 1);
-
-                for (int i = 0; i < enemyCount; i++)
-                {
-                    EnemyShip enemyShip;
-                    if (rowIndex == 0)
-                    {
-                        enemyShip = new EnemyLevel1();
-                    }
-                    else if (rowIndex == 1)
-                    {
-                        enemyShip = new EnemyLevel2();
-                    }
-                    else
-                    {
-                        enemyShip = new EnemyLevel3();
-                    }
-
-                    this.canvas.Children.Add(enemyShip.Sprite);
-                    this.enemyShips.Add(enemyShip);
-
-                    double xPosition = (i + 1) * spacing - enemyShip.Width / 2.0;
-                    enemyShip.X = xPosition;
-                    enemyShip.Y = startY - rowIndex * rowSpacing;
-                }
-            }
-        }
+        
 
         private void placePlayerNearBottomOfBackgroundCentered()
         {
@@ -159,32 +130,13 @@ namespace Galaga.Model
         /// </summary>
         public void MovePlayerRight()
         {
-            if (this.player.X >= this.canvasWidth - this.player.Width - 3)
+            if (this.player.X >= this.canvasWidth - this.player.Width - PlayerSpeedBoundary)
             {
-                this.player.X = this.canvasWidth - this.player.Width - 3;
+                this.player.X = this.canvasWidth - this.player.Width - PlayerSpeedBoundary;
             }
 
             this.player.MoveRight();
-        }
-
-        private void moveEnemyShips()
-        {
-            foreach (var ship in this.enemyShips)
-            {
-                if (this.tickCounter < 10)
-                {
-                    ship.MoveLeft();
-                }
-                else if (this.tickCounter < 30)
-                {
-                    ship.MoveRight();
-                }
-                else if (this.tickCounter < 40)
-                {
-                    ship.MoveLeft();
-                }
-            }
-        }
+        }     
 
         /// <summary>
         /// Fires the missile.
@@ -265,6 +217,5 @@ namespace Galaga.Model
         }
 
         #endregion
-
     }
 }
